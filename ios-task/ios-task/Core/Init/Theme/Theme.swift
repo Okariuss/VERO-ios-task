70947af0a1b587a095085358b397b9b6df0b1c5c
Okan <orkunokann@gmail.com>
2024-02-02 01:57:24 +0300

    Theme created

---
+++ ios-task/ios-task/Core/Init/Theme/Theme.swift
@@ -0,0 +1,23 @@
+//
+//  Theme.swift
+//  ios-task
+//
+//  Created by Okan Orkun on 2.02.2024.
+//
+
+import Foundation
+
+struct Theme {
+    let themeFont: ThemeFont
+}
+
+extension Theme {
+    static var defaultTheme: Theme {
+        return Theme(
+            themeFont: ThemeFont(
+                headlineFont: FontSize.headline.toFont.boldVersion,
+                bodyFont: FontSize.body.toFont
+            )
+        )
+    }
+}
---
+++ ios-task/ios-task/Core/Init/Theme/ThemeFont.swift
@@ -0,0 +1,14 @@
+//
+//  ThemeFont.swift
+//  ios-task
+//
+//  Created by Okan Orkun on 2.02.2024.
+//
+
+import Foundation
+import UIKit
+
+struct ThemeFont {
+    let headlineFont: UIFont
+    let bodyFont: UIFont
+}
