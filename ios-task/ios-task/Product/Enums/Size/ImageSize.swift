596fbe0a070376b1312478362809a31811d646c2
Okan <orkunokann@gmail.com>
2024-02-01 14:46:55 +0300

    Structure error solved

---
+++ ios-task/ios-task/Product/Enums/Size/ImageSize.swift
@@ -0,0 +1,31 @@
+//
+//  ImageSize.swift
+//  ios-task
+//
+//  Created by Okan Orkun on 1.02.2024.
+//
+
+import Foundation
+
+enum SizeEnums {
+    case normal
+    case medium
+    case high
+
+    var rawValue: Int {
+        switch self {
+        case .normal:
+            return 128
+        case .medium:
+            return 256
+        case .high:
+            return 512
+        }
+        
+    }
+    
+    var toSize: CGSize {
+        return CGSize(width: rawValue, height: rawValue)
+    }
+    
+}
